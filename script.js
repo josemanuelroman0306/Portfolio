d337a6056af23c86a327746431f4d2eb9b9c8e4c
José Manuel Román Rodríguez <josemanuelroman0306@gmail.com>
2026-08-19 08:34:09 +0200

    Actualización del Portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+const revealElements = document.querySelectorAll('.reveal');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('active');
+        }
+    });
+}, {
+    threshold: 0.15
+});
+
+revealElements.forEach(element => {
+    observer.observe(element);
+});
